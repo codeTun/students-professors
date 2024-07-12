aec5f54134075ee71f566008fa1084d0aa8aafe8
codeTun <iheblazhary@gmail.com>
2024-07-12 01:27:49 +0100

    🔓 Added auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
 
+type registerRequest = {
+  username: string;
+  email: string;
+  password: string;
+  role: string;
+};
+
+type loginRequest = {
+  email: string;
+  password: string;
+};
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private baseUrl = 'http://localhost:8089/api/auth'; // Adjust the base URL as needed
 
-  constructor() { }
+  constructor() {}
+
+  async login(loginData: loginRequest): Promise<any> {
+    const response = await fetch(`${this.baseUrl}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(loginData),
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    return response.json();
+  }
 }
