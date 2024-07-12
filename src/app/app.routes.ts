011c0c571f4dc2e673af2c8c6235d2dd7c66f009
codeTun <iheblazhary@gmail.com>
2024-07-12 04:58:19 +0100

    📱 Added contact section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { ListCoursesComponent } from '../app/list-courses/list-courses.component
 import { CoursesCardComponent } from './courses-card/courses-card.component';
 import { HomeComponent } from './home/home.component';
 import { CalendarComponent } from './calendar/calendar.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path:'contact', component: ContactComponent},
   { path: 'calendar', component: CalendarComponent},
   { path: 'list-courses', component: ListCoursesComponent },
   { path: 'courses-card', component: CoursesCardComponent },
