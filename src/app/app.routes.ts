c2c0167667387ec1ab78fea4ad0c42602c6b4ed2
codeTun <iheblazhary@gmail.com>
2024-07-12 02:25:39 +0100

    👥 Added list courses | Fixed routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ListCoursesComponent } from '../app/list-courses/list-courses.component'; // Adjust the path as necessary
+import { CoursesCardComponent } from './courses-card/courses-card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'list-courses', component: ListCoursesComponent },
+  {path: 'courses-card', component: CoursesCardComponent}
+];
