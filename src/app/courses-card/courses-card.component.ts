95dc7126371b5dd2032e31a1d2c67366615264f8
codeTun <iheblazhary@gmail.com>
2024-07-12 04:12:59 +0100

    ⏳ Proccessing ..

--- src/app/courses-card/courses-card.component.ts
+++ src/app/courses-card/courses-card.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-courses-card',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './courses-card.component.html',
   styleUrl: './courses-card.component.css'
 })
